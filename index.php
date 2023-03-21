d463766f651908878b6ebb42ead9bf6c9347b4cc
hdamiani <hdamiani2@na-sa.com.ar>
2023-03-21 13:07:56 -0300

    nuevo html

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+	$valor = 10
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php echo $valor;?></title>
+</head>
+<body>
+<?php
+
+
+
+?>
+</body>
+</html>
